92cbd33c7c9104261390c45d013a9a89ea925b53
tengzinc <tengzinc@gmail.com>
2021-03-15 18:25:27 +0800

    Updated!

--- RedditCrawler.py
+++ RedditCrawler.py
@@ -27,35 +27,6 @@ class RedditCrawler(Crawler):
             #     c.author), datetime.utcfromtimestamp(c.created_utc))
         db.disconnect()
 
-
-<<<<<<< HEAD
-# crawler = RedditCrawler("Crimes", "zSqCr7ZeezCMgQ",
-#                         "-K97i2uEaVP9ae69IGGJ8HXp7Xz3LA")
-# crawler.crawl()
-=======
-    def crawl2(self):
-        db = Database()
-        subreddit = self.reddit.subreddit('Singapore')
-        for submission in subreddit.search("jail"):
-            submission.comments.replace_more(limit=None)
-            comment = submission.comments.list()
-            print("P: {} , Title: {} , ups: {} , downs: {} , Comments: {}".format(submission.id,
-                                                                                  submission.title,
-                                                                                  submission.ups,
-                                                                                  submission.downs,
-                                                                                  len(comment)))
-            db.insert(str(submission.id), str(submission.author), str(
-                submission.title), datetime.utcfromtimestamp(submission.created_utc))
-            # retrieve replies
-            for c in comment:
-                if len(comment) > 0:
-                    db.insert(str(c.id), str(c.author), str(
-                        c.body), datetime.utcfromtimestamp(c.created_utc))
-
-
-        db.disconnect()
-
 #crawler = RedditCrawler("Shopping_habits", "zSqCr7ZeezCMgQ",
 #                        "-K97i2uEaVP9ae69IGGJ8HXp7Xz3LA")
 #crawler.crawl()
->>>>>>> 6f5fa3125dd84511bb3f89f4f2580bd4cac3e3af
