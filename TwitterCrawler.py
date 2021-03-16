57b972cb15fdaa25d2d47e073828a8f1ae40cf63
tengzinc <tengzinc@gmail.com>
2021-03-16 15:57:41 +0800

    Updated

--- TwitterCrawler.py
+++ TwitterCrawler.py
@@ -1,5 +1,6 @@
 import tweepy
 from Crawler import Crawler
+# import csv
 from Database import Database
 consumer_key = "OrRuKndlEY6Xx3sOEuWaW3dPx"
 consumer_secret = "hwXN4qFNrCSRTy3k8tZWJ5uqJREGI8gJVDhpJj7YZ5Gs3PLfbL"
@@ -13,15 +14,20 @@ class TwitterCrawler(Crawler):
         self.auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(self.auth)
         self.number_of_tweets = 1000
-    # Crawl function
 
+    # Crawl function
     def crawl(self):
         db = Database()
-        for t in tweepy.Cursor(self.api.user_timeline, id="straits_times", tweet_mode="extended").items(self.number_of_tweets):
-            # print("Text: {}\n Likes: {}\n Date: {}\n User: {}\n Retweet count: {}".format(
-            #     t.full_text, t.favorite_count, t.created_at, t.user.screen_name, t.retweet_count))
-            db.insert("tweet", str(t.user.screen_name), str(t.full_text), t.favorite_count, t.created_at, t.retweet_count)
+        # csvFile = open('result.csv', 'a')
+        # csvWriter = csv.writer(csvFile)
+        search_words = ["charged", "singapore"]
+        for t in self.api.search(q=search_words, tweet_mode='extended', count=1000):
+            # csvWriter.writerow([t.full_text.encode(
+            #     'utf-8'), t.favorite_count, t.created_at, str(t.user.screen_name), t.retweet_count])
+            db.insert("tweet", str(t.user.screen_name), str(
+                t.full_text), t.favorite_count, t.created_at, t.retweet_count)
         db.disconnect()
 
-#crawler = TwitterCrawler()
-#crawler.crawl()
+
+crawler = TwitterCrawler()
+crawler.crawl()
