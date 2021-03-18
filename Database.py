510ff9627709e6b935faadcbd43357c40e1ff12c
h5119uy <ojlasiol@gmail.com>
2021-03-18 14:07:07 +0800

    update db

--- Database.py
+++ Database.py
@@ -2,16 +2,36 @@ import mysql.connector
 from mysql.connector import Error
 
 class Database:
+    """
+    A class used to represent Database
+    '''
+    Attributes
+    ----------
+    self._db : Object
+        Database connection object to access database
 
-    def __init__(self):
-        self.host = "localhost"
-        self.user = "root"
-        self.password = "password" #change if password is different
-        self.database = "sqldatabase" #change if different database
-        self.charset = "utf8mb4" #
+    """
+
+    def __init__(self,host,user,password,database):
+        """
+         Construct all the necessary attributes for Database and initialize the logins
+        :param host: str. server of the database
+        :param user: str. username of the database
+        :param password: str. password of the database
+        :param database: str. Schema of the database
+        """
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.charset = "utf8mb4"
         self._connection() #call _connection function
 
-    def _connection(self):
+    def _connection(self): #private method
+        """
+            Create connection between python and mysql
+            ----------
+        """
         try:
             conn = mysql.connector.connect(
                 host = self.host,
@@ -25,53 +45,126 @@ class Database:
             print("error while connecting to database",err)
 
     def createTable(self): #not required anymore as the database is inside .sql script
+        """
+        Create Table in the database, if table exist it will truncate table to clear data previously
+        :return:
+        """
         cursor = self._db.cursor()
-        #cursor.execute(("CREATE TABLE IF NOT EXISTS commenteds(category VARCHAR(50) NOT NULL,user VARCHAR(20) NOT NULL,commentedd VARCHAR(5000) NOT NULL)")) #checked
-        cursor.execute("CREATE TABLE crawleddata (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, type VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,user VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,"
-                       "text VARCHAR(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL NULL,"
-                       "likes INT , dates DATETIME , commented INT )")
-        #id INT NOT NULL AUTO_INCREMENT
-        self._db.commit() #to update database
+        sql = "CREATE TABLE crawleddata (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, type VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,user VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,"\
+              "text VARCHAR(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL NULL,"\
+              "likes INT , dates DATETIME , commented INT )"
+        #cursor.execute("CREATE TABLE IF NOT EXISTS crawleddata (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, type VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,user VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,"
+                       #"text VARCHAR(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL NULL,"
+                       #"likes INT , dates DATETIME , commented INT )")
+        try:
+            cursor.execute(sql)
+            self._db.commit() #to update database
+        except Error as err:
+            print("error creating table,",err)
+            self.truncatetable()
+            print("table truncated")
         cursor.close()
-        pass
 
     def disconnect(self): # put _ to make it private
+        """
+        Disconnect from database
+        """
         self._db.close()
         print("connection close")
 
-    def insert(self, type, user, text,likes,datecreate,commented): # change to 4parameter
+    def insert(self, type, user, text,likes,datecreate,commented): # TAKES IN 6 PARAMETER
+        """
+        inserting data into MYSQL database
+        :param type: str. type of post (reddit or twitter)
+        :param user: str. user that created the post
+        :param text: str. content of the post
+        :param likes: str. the amount of likes for the post
+        :param datecreate: str. the date and time of the post that was created
+        :param commented: str. number of comments for the post
+        """
         #enter whatever or grab whatever put inside here to insert
         cursor = self._db.cursor() #access sql
         val = (type, user,text,likes,datecreate,commented)
-        #print(val)
-        cursor.execute("INSERT INTO crawleddata (type,user,text,likes,dates,commented) VALUES (%s, %s,%s, %s, %s,%s)", val)
-        self._db.commit() #update database
-        #print("record inserted.")
+        try:
+            cursor.execute("INSERT INTO crawleddata (type,user,text,likes,dates,commented) VALUES (%s, %s,%s, %s, %s,%s)", val)
+            self._db.commit() #update database
+        except Error as err:
+            print("error inserting data into database.",err)
         cursor.close() #close cursor everytime after use for security purposes
 
     def read(self,limit):
+        """
+        Read top few data from table using limits
+        :param limit: int
+        :return:
+         result: str
+        """
         cursor = self._db.cursor()
-        cursor.execute("SELECT * FROM crawleddata LIMIT "+(str(limit))) #select all from table(comment)
-        result = cursor.fetchall()
-        for x in result:
-           print(x)
-        # for x in result:
-        #     print(x)
+        try:
+            cursor.execute("SELECT * FROM crawleddata LIMIT "+(str(limit))) #select all from table(comment)
+            result = cursor.fetchall()
+        except Error as err:
+            print("error reading data",err)
+        #for x in result:
+         #  print(x)
         cursor.close()
         return result
 
     def search(self,keyword):
+        """
+        search keyword to look for news
+        :param keyword: str
+        :return:
+         result: str
+        """
         cursor = self._db.cursor()
         sql = "SELECT * FROM crawleddata WHERE text LIKE \"%"+keyword+"%\""
-        cursor.execute(sql)
-        x = cursor.fetchall()
-        for result in x:
-            print(result) #print result here
+        #ALTER TABLE crawleddata ADD FULLTEXT (text);
+        #sql =  "SELECT * FROM crawleddata WHERE MATCH(text) AGAINST ('" +keyword +"')"
+        try:
+            cursor.execute(sql)
+        except Error as err:
+            print("error searching for keyword. ",err)
+        result = cursor.fetchall()
+        #for result in x:
+         #   print(result) #print result here
         cursor.close()
-
+        return result
 
     def truncatetable(self):
+        """
+        to remove item from table(crawleddata) that was saved previously
+        """
         cursor = self._db.cursor()
-        cursor.execute("TRUNCATE TABLE crawleddata")  # THIS IS TO REMOVE ITEM FROM THE TABLE SAVED PREVIOUSLY
+        try:
+            cursor.execute("TRUNCATE TABLE crawleddata")  # THIS IS TO REMOVE ITEM FROM THE TABLE SAVED PREVIOUSLY
+        except Error as err:
+            print("error deleteing table",err)
         # updating code here
         cursor.close()
+
+    def printnews(self,category): #print specific news
+        """
+        Printing news base on social media (reddit or twitter)
+        :param category: str
+        :return:
+        result: str
+        """
+        cursor = self._db.cursor()
+        if(category == "twitter"):
+            type = "tweet"
+        elif(category == "reddit"):
+            type = "post"
+        else:
+            print("type only twitter or reddit")
+            exit()
+        sql = "SELECT * FROM crawleddata WHERE type = '"+type+"'"
+        try:
+            cursor.execute(sql)   # select all from table(comment)
+        except Error as err:
+            print("error printing news",err)
+        result = cursor.fetchall()
+        #for x in result:
+         #   print(x)
+        cursor.close()
+        return result
--- SentimentAnalyzer.py
+++
@@ -1,7 +0,0 @@
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-class SentimentAnalyzer:
-    comments={}
-
-    def analyzeSentiment(text):
-        score= SentimentIntensityAnalyzer().polarity_scores(text)
-        return score['compound']
